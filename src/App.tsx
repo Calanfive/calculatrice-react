76b4dad9bd6d560fb55b66d78fb7851ac5dd3ca9
calanfive <camille.lelan@gmail.com>
2023-11-08 10:17:59 +0100

    create children

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,25 @@ function App() {
     <>
       <div className='calculatrice'>
         <input className='input'></input>
-        <div className='ligneUne'>
+        <div className='ligneUne ligne'>
           <button>7</button>
           <button>8</button>
           <button>9</button>
           <button>-</button>
         </div>
-        <div className='ligneDeux'>
+        <div className='ligneDeux ligne'>
           <button>4</button>
           <button>5</button>
           <button>6</button>
           <button>+</button>
         </div>
-        <div className='ligneTrois'>
+        <div className='ligneTrois ligne'>
           <button>1</button>
           <button>2</button>
           <button>3</button>
           <button>=</button>
         </div>
-        <div className='ligneQuatre'>
+        <div className='ligneQuatre ligne'>
           <button>0</button>
         </div>
       </div>
